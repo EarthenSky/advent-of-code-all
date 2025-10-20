e138c174d6e5cc96bc2562543c2f63487a89c645
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2025-10-20 16:34:24 -0700

    (2016) complete day 7 part 2

--- 2016/day7.c
+++ 2016/day7.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <iso646.h>
 
@@ -13,7 +15,6 @@ void part1(const struct file_info *fi) {
     uint32_t num_addresses_supporting_tls = 0;
     size_t ci = 0;
     while (ci < fi->num_bytes) {
-        // printf("num_addresses_supporting_tls = %u (ci = %zu) (num bytes = %zu)\n", num_addresses_supporting_tls, ci, fi->num_bytes);
         bool has_abba_outside = false;
         bool has_abba_inside = false;
         bool in_hypernet_sequence = false;
@@ -74,8 +75,96 @@ void part1(const struct file_info *fi) {
 
     printf("num addresses supporting TLS = %u\n", num_addresses_supporting_tls);
 }
-void part2() {
 
+struct ab {
+    char a;
+    char b;
+};
+
+void part2(const struct file_info *fi) {
+    printf("part 2:\n");
+
+    uint32_t num_addresses_supporting_ssl = 0;
+    size_t ci = 0;
+    while (ci < fi->num_bytes) {
+        const char* end_of_line = strstr(fi->file_bytes + ci, "\n");
+        if (end_of_line == NULL)
+            end_of_line = strstr(fi->file_bytes + ci, "\0");
+        
+        if (end_of_line == NULL) {
+            fprintf(stderr, "ERROR: string malformed\n");
+            exit(EXIT_FAILURE);
+        }
+    
+        // allocate bytes equal to the size of this line, just in case
+        ptrdiff_t line_size = end_of_line - (fi->file_bytes + ci);
+        size_t outside_list_size = 0;
+        struct ab *outside_list = malloc(line_size * sizeof *outside_list);
+        size_t inside_list_size = 0;
+        struct ab *inside_list = malloc(line_size * sizeof *inside_list);
+
+        bool in_hypernet_sequence = false;
+
+        // fill inside & outside lists
+        while (true) {
+            char ch = fi->file_bytes[ci];
+            if (ch == '\n' || ch == '\0') {
+                break;
+            } else if (ch == '[') {
+                in_hypernet_sequence = true;
+            } else if (ch == ']') {
+                in_hypernet_sequence = false;
+            } else if (not (ch >= 'a' and ch <= 'z')) {
+                fprintf(stderr, "ERROR: got unexpected char `%c` (%d)\n", ch, ch);
+                exit(EXIT_FAILURE);
+            } else if (
+                ci >= 2
+                and fi->file_bytes[ci-1] != '\n'
+                and fi->file_bytes[ci-2] != '\n'
+                and fi->file_bytes[ci-1] != '['
+                and fi->file_bytes[ci-2] != '['
+                and fi->file_bytes[ci-1] != ']'
+                and fi->file_bytes[ci-2] != ']'
+            ) {
+                char a = fi->file_bytes[ci-2];
+                char b = fi->file_bytes[ci-1];
+                char c = fi->file_bytes[ci-0];
+
+                bool is_aba = a != b and a == c;
+                if (is_aba and in_hypernet_sequence) {
+                    inside_list[inside_list_size] = (struct ab) { .a = a, .b = b };
+                    inside_list_size += 1;
+                } else if (is_aba and !in_hypernet_sequence) {
+                    outside_list[outside_list_size] = (struct ab) { .a = a, .b = b };
+                    outside_list_size += 1;
+                }
+            }
+
+            ci += 1;
+        }
+
+        // skip the final newline
+        ci += 1;
+
+        // check whether there exists an ab outside where ba is inside, then add to ssl count 
+        for (size_t oi = 0; oi < outside_list_size; oi++) {
+            for (size_t ii = 0; ii < inside_list_size; ii++) {
+                if (
+                    outside_list[oi].a == inside_list[ii].b
+                    and outside_list[oi].b == inside_list[ii].a
+                ) {
+                    num_addresses_supporting_ssl += 1;
+                    goto DONE_CHECKING_SSL;
+                }
+            }
+        }
+
+        DONE_CHECKING_SSL:
+        free(outside_list);
+        free(inside_list);
+    }
+
+    printf("num addresses supporting SSL = %u\n", num_addresses_supporting_ssl);
 }
 
 int main() {
